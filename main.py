9248788b86c2d4e2567f2a7a8f9f6badb69fcf61
Shashank <shashankr8724@gmail.com>
2024-01-28 20:19:25 +0530

    minor updates

--- main.py
+++ main.py
@@ -1,27 +1,52 @@
 from fastapi import FastAPI
 from google.oauth2 import service_account
 from googleapiclient.discovery import build
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-SCOPES = ['https://www.googleapis.com/auth/drive.readonly']
+origins = [
+    "http://localhost",
+    "http://localhost:8000",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+SCOPES = ["https://www.googleapis.com/auth/drive.readonly"]
 ref = "1vKcGrpCAs5-V4iJBbdG1YhoueQAmcwDP"
+
 
 def authenticate_with_service_account():
     credentials = service_account.Credentials.from_service_account_file(
-        'sample-411214-09943b2e9710.json', scopes=SCOPES)
+        "sample-411214-09943b2e9710.json", scopes=SCOPES
+    )
     return credentials
+
 
 def generate_file_url(file):
     url = f"https://drive.google.com/file/d/{file['id']}/view"
     return url
 
+
 def list_files_in_folder(folder_id, credentials):
-    service = build('drive', 'v3', credentials=credentials)
-    results = service.files().list(
-        q=f"'{folder_id}' in parents and trashed=false",
-        fields="files(id, name, mimeType)").execute()
-    files = results.get('files', [])
+    service = build("drive", "v3", credentials=credentials)
+    results = (
+        service.files()
+        .list(
+            q=f"'{folder_id}' in parents and trashed=false",
+            fields="files(id, name, mimeType)",
+            orderBy="folder, name",
+        )
+        .execute()
+    )
+    files = results.get("files", [])
 
     for i in files:
         if i["mimeType"] == "application/vnd.google-apps.folder":
@@ -31,12 +56,15 @@ def list_files_in_folder(folder_id, credentials):
             i["url"] = generate_file_url(i)
     return files
 
+
 credentials = authenticate_with_service_account()
+
 
 @app.get("/getinitial/")
 async def getinitial():
-    return list_files_in_folder(ref, credentials)
+    return {"id": ref, "data": list_files_in_folder(ref, credentials)}
+
 
 @app.get("/getfiles/{id}")
 async def getfiles(id: str):
-    return list_files_in_folder(id, credentials)
+    return {"id": id, "data": list_files_in_folder(id, credentials)}
